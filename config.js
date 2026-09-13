ad8f6cb4c0f63de988d56a07e3d2b9e887331a4c
kostklsh-maker <kostklsh@gmail.com>
2026-09-13 20:31:29 +0300

    Update API_URL in config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // ============================================================
 // Единое место для хранения API_URL для всего проекта
 // ============================================================
-const API_URL = "https://script.google.com/macros/s/AKfycbzpn1HKWzkYfQaeWbkN7VwrVF63zO0Q0CDsAEzdkUBt2TDGpSXdyzX8fVhgKxLFI5mkbQ/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbzuZk-znNb4QkaPMickfp7C4Rim6NMhlxdRQiiOKJpjCjZZUFM_0VnO5ryG5kZk-nXE/exec";
 // Замените YOUR_DEPLOYMENT_ID на ваш реальный ID из Apps Script
