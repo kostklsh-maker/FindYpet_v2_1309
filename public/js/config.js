e1fe10ce503f6b47915b4bb508db65685653b5ed
kostklsh-maker <kostklsh@gmail.com>
2026-09-13 17:33:41 +0300

    Delete public directory

--- public/js/config.js
+++
@@ -1,5 +0,0 @@
-// ============================================================
-// Единое место для хранения API_URL для всего проекта
-// ============================================================
-const API_URL = "https://script.google.com/macros/s/AKfycbzpn1HKWzkYfQaeWbkN7VwrVF63zO0Q0CDsAEzdkUBt2TDGpSXdyzX8fVhgKxLFI5mkbQ/exec";
-// Замените YOUR_DEPLOYMENT_ID на ваш реальный ID из Apps Script
